acdb292ae492c6067c1abf7a60847f6e552706a6
Mike Taykle <miketaykle@o5p.me>
2022-02-22 04:59:49 -0500

    update

---
+++ tests/bootstrap.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * WP Groove™ {@see https://wpgroove.com}
+ *  _       _  ___       ___
+ * ( )  _  ( )(  _`\    (  _`\
+ * | | ( ) | || |_) )   | ( (_) _ __   _      _    _   _    __  ™
+ * | | | | | || ,__/'   | |___ ( '__)/'_`\  /'_`\ ( ) ( ) /'__`\
+ * | (_/ \_) || |       | (_, )| |  ( (_) )( (_) )| \_/ |(  ___/
+ * `\___x___/'(_)       (____/'(_)  `\___/'`\___/'`\___/'`\____)
+ */
+// <editor-fold desc="Strict types, namespace, use statements, and other headers.">
+
+/**
+ * Lint configuration.
+ *
+ * @since 2021-12-25
+ *
+ * phpcs:disable
+ */
+
+/**
+ * Declarations & namespace.
+ *
+ * @since 2021-12-25
+ */
+declare( strict_types = 1 );
+namespace WP_Groove\LiteSpeed_Downloads_For_WooCommerce\Tests;
+
+// </editor-fold>
+
+/**
+ * Bootstrap using closure from WP Groove framework.
+ *
+ * @since 2022-02-21
+ */
+( require __DIR__ . '/vendor/clevercanyon/wpgroove-framework/tests/bootstrap-closure.php' )( __FILE__, 'plugin' );
